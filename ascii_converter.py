3552a2a4d2c8220adf4a9ab94a50f3f47c070c2f
Syahid <92115178+syahidhusein@users.noreply.github.com>
2022-11-21 23:49:32 +0100

    Delete ascii_converter.py

--- ascii_converter.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def min_max_scaling(array):
-    """Scales all values in array by the min and max value.
-    """
-    min_value = np.min(array)
-    max_value = np.max(array)
-    return (array- min_value)/(max_value - min_value)
-
